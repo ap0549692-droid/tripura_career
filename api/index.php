9ec20f6eb1463a823d1a36ee2ed82cf1a8fc9716
ap0549692-droid <ap0549692@gmail.com>
2026-09-13 00:49:36 +0530

    Add environment variable configurations
    Set environment variables for caching, sessions, and queues.

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+putenv('CACHE_STORE=array');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=array');
+putenv('QUEUE_CONNECTION=sync');
+putenv('BROADCAST_CONNECTION=log');
+putenv('FILESYSTEM_DISK=public');
+$_ENV['CACHE_STORE']='array';
+$_ENV['SESSION_DRIVER']='array';
 require __DIR__ . '/../public/index.php';
