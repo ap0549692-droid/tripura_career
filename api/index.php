2c6efcf8757a5ea35b3ee5b1c5225f5f8723fa42
ap0549692-droid <ap0549692@gmail.com>
2026-09-13 00:52:44 +0530

    Update environment variables and require statements
    Added SESSION_STORE environment variable and updated require paths.

--- api/index.php
+++ api/index.php
@@ -2,9 +2,16 @@
 putenv('CACHE_STORE=array');
 putenv('CACHE_DRIVER=array');
 putenv('SESSION_DRIVER=array');
+putenv('SESSION_STORE=array');
 putenv('QUEUE_CONNECTION=sync');
 putenv('BROADCAST_CONNECTION=log');
 putenv('FILESYSTEM_DISK=public');
-$_ENV['CACHE_STORE']='array';
-$_ENV['SESSION_DRIVER']='array';
-require __DIR__ . '/../public/index.php';
+
+$_ENV['CACHE_STORE'] = 'array';
+$_ENV['CACHE_DRIVER'] = 'array';
+$_ENV['SESSION_DRIVER'] = 'array';
+$_ENV['QUEUE_CONNECTION'] = 'sync';
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+return $app->handleRequest(Illuminate\Http\Request::capture());
